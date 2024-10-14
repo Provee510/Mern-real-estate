7d96deff9027f158fd604516029e6d3950fb2890
provee510 <providencecharles@yahoo.com>
2024-10-14 10:52:55 -0700

    added redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// // import { createRoot } from 'react-dom/client';
+// import App from './App.jsx';
+// import './index.css';
+// import { store } from './redux/store.js';
+// import { Provider } from'react-redux';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// ReactDom.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+// )
+
+
+
+
+
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store } from './redux/store.js';
+import { Provider } from 'react-redux';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
